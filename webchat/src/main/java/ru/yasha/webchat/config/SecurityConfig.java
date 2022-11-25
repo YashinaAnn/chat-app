69f1c46ac82a2aaacfafcd88059752dd57241182
Anna Yashina <ayashina@Annas-MacBook-Pro.local>
2022-11-25 19:41:08 +0400

    Added security

---
+++ webchat/src/main/java/ru/yasha/webchat/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.yasha.webchat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(config -> config
+                        .anyRequest().authenticated())
+                .oauth2Login()
+                .and()
+                .logout(config -> config
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"))
+                .build();
+    }
+}
